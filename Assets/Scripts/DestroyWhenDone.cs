904856bfe575e39cef8b829f5d2e7dd9aa30276f
James Liu <contact@jamessliu.com>
2018-07-14 17:11:36 -0700

    Update

---
+++ Assets/Scripts/DestroyWhenDone.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyWhenDone : MonoBehaviour {
+
+  public Animator Animator;
+  public AudioSource AudioSource;
+	
+	// Update is called once per frame
+	void Update () {
+    bool keepGameObject = false;
+    if (AudioSource != null) {
+      keepGameObject |= AudioSource.isPlaying;
+    }
+    if (Animator != null)  {
+      keepGameObject |= Animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1f;
+    }
+    if (!keepGameObject) {
+      Destroy(gameObject);
+    }
+	}
+
+}
---
+++ Assets/Scripts/ReactorHealthUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReactorHealthUI : MonoBehaviour {
+
+  public Reactor Reactor;
+  public Image HealthBar;
+  public Image Nuclear;
+
+  public float MaxSinusoidSpeed = 5;
+  public Gradient NuclearGradient;
+
+	// Use this for initialization
+	void Start () {
+    if (Reactor == null) {
+      Reactor = GameObject.FindWithTag("Reactor").GetComponent<Reactor>();
+    }
+	}
+
+  /// <summary>
+  /// Update is called every frame, if the MonoBehaviour is enabled.
+  /// </summary>
+  void Update() {
+    UpdateHealthBar();
+    UpdateNuclear();
+  }
+
+  void UpdateHealthBar() {
+    if (HealthBar == null) return;
+    HealthBar.fillAmount = Reactor.ReactorCurrentHealth / Reactor.ReactorHealth;
+  }
+
+  void UpdateNuclear() {
+    if (Nuclear == null) return;
+    var fraction = 1 - Reactor.ReactorCurrentHealth / Reactor.ReactorHealth;
+    var sinusoid = Mathf.Sin(Time.unscaledTime * MaxSinusoidSpeed * fraction);
+    var amount =  sinusoid / 2 + 0.5f;
+    Nuclear.color = NuclearGradient.Evaluate(amount);
+  }
+
+
+}
